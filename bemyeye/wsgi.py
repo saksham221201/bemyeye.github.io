6226a4de844781b198615f2d70aaed80e03d4fb3
Sanchit Gupta <sanchitgupta0101@gmail.com>
2022-10-24 11:04:41 +0530

    Update wsgi.py

--- bemyeye/wsgi.py
+++ bemyeye/wsgi.py
@@ -1,7 +1,6 @@
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bemyeye.settings")
-from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
